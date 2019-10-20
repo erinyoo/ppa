a9180b8b8c68e978badcb170ecad8b78629a794b
erinyoo <erinyoo@ufl.edu>
2019-10-20 04:27:34 -0400

    Cleaned up code, finished database tests

--- tests/database/db.test.js
+++ tests/database/db.test.js
@@ -5,7 +5,9 @@ var sinon = require('sinon');
 process.env.NODE_ENV = 'test';
 
 var bmiController = require('../../src/controllers/bmi.js');
+var distanceController = require('../../src/controllers/distance.js');
 var bmiFix = require('./fixtures/bmi.json');
+var distanceFix = require('./fixtures/distance.json');
 var BMI = require('../../src/models/bmi.js');
 var Distance = require('../../src/models/distance.js');
 
@@ -51,6 +53,54 @@ describe('BMI Database Functions', () => {
         };
         bmiController.sendBMI(req, res);
         expect(BMI.prototype.save.callCount).to.equal(1);
+        done();
+    });
+});
+
+describe('Distance Database Functions', () => {
+    beforeEach(() => {
+        sinon.stub(Distance, 'find');
+        sinon.stub(Distance.prototype, 'save');
+    });
+
+    afterEach(() => {
+        Distance.find.restore();
+        Distance.prototype.save.restore();
+    });
+
+    it('should retreive all the Distance docs', (done) => {
+        var obj = distanceFix.all.success.res;
+        Distance.find.yields(null, obj);
+        var req = {
+            body: {
+                x1: 5,
+                y1: 3,
+                x2: 1,
+                y2: 10
+            }
+        };
+        var res = {
+            send: sinon.stub()
+        };
+        distanceController.getDistance(req, res);
+        sinon.assert.calledWith(res.send, obj);
+        done();
+    });
+
+    it('should save Distance docs', (done) => {
+        var req = {
+            body: {
+                x1: 5,
+                y1: 3,
+                x2: 1,
+                y2: 10
+            }
+        };
+        var res = {
+            send: sinon.stub()
+        };
+        distanceController.sendDistance(req, res);
+        expect(Distance.prototype.save.callCount).to.equal(1);
         done();
     });
 });
